d249cbcce72a9d38678c53a5d6c50dad0296e7b9
Muhammad Bin Khalid <chmuhammadbinkhalid28@gmail.com>
2025-03-06 23:21:43 +0500

    Update

--- app/src/main/java/com/example/portalmbktechstudio/MainActivity.java
+++ app/src/main/java/com/example/portalmbktechstudio/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "WebViewDebug";
 
     // URLs and version constants
-    private static String MAIN_URL = "https://portal.mbktechstudio.com/";
+    private static String MAIN_URL = "https://portalapp.mbktechstudio.com/";
     private static final String REST_API_URL = "https://api.mbktechstudio.com/api/poratlAppVersion";
     private static final String REDIRECT_URL = "https://mbktechstudio.com";
     private static final String CURRENT_VERSION = "1.2.1";
